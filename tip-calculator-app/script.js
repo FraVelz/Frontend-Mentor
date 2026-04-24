3dc0c30a10b892773fd911ade118bb1be6985bca
Fravelz <fravelz_@hotmail.com>
2026-04-24 15:26:34 -0500

    fix(general): details and issues (part #1)

--- tip-calculator-app/script.js
+++ tip-calculator-app/script.js
@@ -1,95 +1,95 @@
-const inputs = document.querySelectorAll(".input-wrapper input");
-const tipButtons = document.querySelectorAll("#tip-options button");
-const tipInput = document.querySelector("#tip-options input");
-const bill = document.getElementById("bill");
-const peopleAmount = document.getElementById("people");
-const tipAmount = document.getElementById("tip-amount");
-const total = document.getElementById("total");
-const resetBtn = document.getElementById("reset-btn");
+const inputs = document.querySelectorAll('.input-wrapper input')
+const tipButtons = document.querySelectorAll('#tip-options button')
+const tipInput = document.querySelector('#tip-options input')
+const bill = document.getElementById('bill')
+const peopleAmount = document.getElementById('people')
+const tipAmount = document.getElementById('tip-amount')
+const total = document.getElementById('total')
+const resetBtn = document.getElementById('reset-btn')
 
-let tipPercentage = 0;
+let tipPercentage = 0
 
 function calculateResults() {
   if (bill.value > 0 && peopleAmount.value > 0) {
-    const tipPerPerson = (bill.value * tipPercentage) / peopleAmount.value;
-    const totalPerPerson = tipPerPerson + bill.value / peopleAmount.value;
-    tipAmount.innerText = `$${tipPerPerson.toFixed(2)}`;
-    total.innerText = `$${totalPerPerson.toFixed(2)}`;
-    resetBtn.disabled = false;
+    const tipPerPerson = (bill.value * tipPercentage) / peopleAmount.value
+    const totalPerPerson = tipPerPerson + bill.value / peopleAmount.value
+    tipAmount.innerText = `$${tipPerPerson.toFixed(2)}`
+    total.innerText = `$${totalPerPerson.toFixed(2)}`
+    resetBtn.disabled = false
   } else {
-    resetResults();
+    resetResults()
   }
 }
 
 function resetInputs() {
-  tipPercentage = 0;
+  tipPercentage = 0
   inputs.forEach((inputEl) => {
-    inputEl.value = "";
-    delete inputEl.dataset.invalid;
-    const formInput = inputEl.closest(".form-input");
-    formInput.querySelector(".error-msg").hidden = true;
-  });
+    inputEl.value = ''
+    delete inputEl.dataset.invalid
+    const formInput = inputEl.closest('.form-input')
+    formInput.querySelector('.error-msg').hidden = true
+  })
   tipButtons.forEach((btn) => {
-    delete btn.dataset.selected;
-  });
-  tipInput.value = "";
+    delete btn.dataset.selected
+  })
+  tipInput.value = ''
 }
 
 function resetResults() {
-  tipAmount.innerText = "$0.00";
-  total.innerText = "$0.00";
-  resetBtn.disabled = true;
+  tipAmount.innerText = '$0.00'
+  total.innerText = '$0.00'
+  resetBtn.disabled = true
 }
 
 inputs.forEach((inputEl) => {
-  inputEl.addEventListener("input", () => {
-    const formInput = inputEl.closest(".form-input");
-    const errorMsg = formInput.querySelector(".error-msg");
+  inputEl.addEventListener('input', () => {
+    const formInput = inputEl.closest('.form-input')
+    const errorMsg = formInput.querySelector('.error-msg')
 
-    if (inputEl.value === "0") {
-      inputEl.dataset.invalid = "true";
-      errorMsg.hidden = false;
+    if (inputEl.value === '0') {
+      inputEl.dataset.invalid = 'true'
+      errorMsg.hidden = false
     } else {
-      delete inputEl.dataset.invalid;
-      errorMsg.hidden = true;
+      delete inputEl.dataset.invalid
+      errorMsg.hidden = true
     }
 
-    calculateResults();
-  });
-});
+    calculateResults()
+  })
+})
 
 tipButtons.forEach((tipBtn) => {
-  tipBtn.addEventListener("click", () => {
-    if (tipBtn.dataset.selected === "true") {
-      delete tipBtn.dataset.selected;
-      tipPercentage = 0;
+  tipBtn.addEventListener('click', () => {
+    if (tipBtn.dataset.selected === 'true') {
+      delete tipBtn.dataset.selected
+      tipPercentage = 0
     } else {
       tipButtons.forEach((btn) => {
-        delete btn.dataset.selected;
-      });
-      tipInput.value = "";
-      tipBtn.dataset.selected = "true";
-      tipPercentage = tipBtn.value / 100;
+        delete btn.dataset.selected
+      })
+      tipInput.value = ''
+      tipBtn.dataset.selected = 'true'
+      tipPercentage = tipBtn.value / 100
     }
 
-    calculateResults();
-  });
-});
+    calculateResults()
+  })
+})
 
-tipInput.addEventListener("input", () => {
-  if (tipInput.value != "") {
+tipInput.addEventListener('input', () => {
+  if (tipInput.value != '') {
     tipButtons.forEach((btn) => {
-      delete btn.dataset.selected;
-    });
-    tipPercentage = tipInput.value / 100;
+      delete btn.dataset.selected
+    })
+    tipPercentage = tipInput.value / 100
   } else {
-    tipPercentage = 0;
+    tipPercentage = 0
   }
 
-  calculateResults();
-});
+  calculateResults()
+})
 
-resetBtn.addEventListener("click", () => {
-  resetInputs();
-  resetResults();
-});
+resetBtn.addEventListener('click', () => {
+  resetInputs()
+  resetResults()
+})
